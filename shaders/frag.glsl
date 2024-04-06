14a2816491cb44e7f1269982643ff079d5c1e0cb
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2024-04-06 19:49:26 +0200

    shader optimizations

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -59,105 +59,71 @@ bool isCoordOutsideCanvas(vec3 coord)
     else return false;
 }
 
-vec3 VoxelTrace(vec3 eye, vec3 marchingDirection, out int stepsTraced)
+vec3 VoxelTrace(vec3 eye, vec3 dir, out int steps)
 {
-    vec3 origin = eye;
-    vec3 direction = marchingDirection;
-    vec3 tdelta;
-    float tx, ty, tz;
+    vec3 stepsize;
+    vec3 toboundry;
 
-    // initialize t
-    if (direction.x < 0)
-    {
-        tdelta.x = -1 / direction.x;
-        tx = (floor(origin.x / 1) * 1 - origin.x) / direction.x;
-    }
-    else 
-    {
-        tdelta.x = 1 / direction.x;
-        tx = ((floor(origin.x / 1) + 1) * 1 - origin.x) / direction.x;
-    }
-    if (direction.y < 0) 
-    {
-        tdelta.y = -1 / direction.y;
-        ty = (floor(origin.y / 1) * 1 - origin.y) / direction.y;
-    }
-    else 
-    {
-        tdelta.y = 1 / direction.y;
-        ty = ((floor(origin.y / 1) + 1) * 1 - origin.y) / direction.y;
-    }
-    if (direction.z < 0)
-    {
-        tdelta.z = -1 / direction.z;
-        tz = (floor(origin.z / 1) * 1 - origin.z) / direction.z;
-    }
-    else
-    {
-        tdelta.z = 1 / direction.z;
-        tz = ((floor(origin.z / 1) + 1) * 1 - origin.z) / direction.z;
-    }
+    // init stepsize
+    stepsize = 1 / abs(dir);
 
-    // initializing some variables
-    float t = 0;
-    int steps = 0;
-    vec3 coord = floor(origin);
-    vec3 result;
-
+    // init toboundry
+    if (dir.x < 0) toboundry.x = (floor(eye.x / 1) - eye.x) / dir.x;
+    else toboundry.x = ((floor(eye.x / 1) + 1) - eye.x) / dir.x;
+    if (dir.y < 0) toboundry.y = (floor(eye.y / 1) - eye.y) / dir.y;
+    else toboundry.y = ((floor(eye.y / 1) + 1) - eye.y) / dir.y;
+    if (dir.z < 0) toboundry.z = (floor(eye.z / 1) - eye.z) / dir.z;
+    else toboundry.z = ((floor(eye.z / 1) + 1) - eye.z) / dir.z;
+    
     // tracing the grid
+    vec3 result;
+    vec3 coord = floor(eye);
     while (true)
     {
-        // if voxel is found
-        if (Sample(coord) != vec3(0))
-        {
-            result = coord;
-            stepsTraced = steps;
-            break;
-        }
-
         // increment step
-        if (tx < ty)
+        if (toboundry.x < toboundry.y)
         {
-            if (tx < tz)
+            if (toboundry.x < toboundry.z)
             {
-                t = tx;
-                tx += tdelta.x;
-                if (direction.x < 0) coord.x -= 1;
+                toboundry.x += stepsize.x;
+                if (dir.x < 0) coord.x -= 1;
                 else coord.x += 1;
             }
             else
             {
-                t = tz;
-                tz += tdelta.z;
-                if (direction.z < 0) coord.z -= 1;
+                toboundry.z += stepsize.z;
+                if (dir.z < 0) coord.z -= 1;
                 else coord.z += 1;
             }
         }
         else
         {
-            if (ty < tz)
+            if (toboundry.y < toboundry.z)
             {
-                t = ty;
-                ty += tdelta.y;
-                if (direction.y < 0) coord.y -= 1;
+                toboundry.y += stepsize.y;
+                if (dir.y < 0) coord.y -= 1;
                 else coord.y += 1;
             }
             else
             {
-                t = tz;
-                tz += tdelta.z;
-                if (direction.z < 0) coord.z -= 1;
+                toboundry.z += stepsize.z;
+                if (dir.z < 0) coord.z -= 1;
                 else coord.z += 1;
             }
         }
-
         steps++;
+
+        // if voxel is found
+        if (Sample(coord) != vec3(0))
+        {
+            result = coord;
+            break;
+        }
 
         // if no voxel was hit or coord is outside the canvas
         if (steps > voxelTraceSteps || (canvasAABBcheck && isCoordOutsideCanvas(coord)))
         {
             result = vec3(0);
-            stepsTraced = steps;
             break;
         }
     }
@@ -167,35 +133,22 @@ vec3 VoxelTrace(vec3 eye, vec3 marchingDirection, out int stepsTraced)
 
 vec3 VoxelNormal(vec3 coord)
 {
-    vec3 normal = vec3(0, 0, 0);
-    int samplesize = 5;
-    float t = samplesize / 2;
-    
-    for (int x = 0; x < samplesize; x++)
+    int samples = 3;
+    int box = samples * 2 + 1;
+
+    vec3 normal = vec3(0);
+    for (int x = -samples; x <= samples; x++)
     {
-        for (int y = 0; y < samplesize; y++)
+        for (int y = -samples; y <= samples; y++)
         {
-            for (int z = 0; z < samplesize; z++)
+            for (int z = -samples; z <= samples; z++)
             {
-                float a = x - t;
-                float b = y - t;
-                float c = z - t;
-                if (Sample(vec3(coord.x + a, coord.y + b, coord.z + c)) != vec3(0))
-                {
-                    normal += vec3(a, b, c);
-                }
+                vec3 offset = vec3(x, y, z);
+                if (offset != vec3(0) && Sample(coord + offset) != vec3(0)) normal += offset;
             }
         }
     }
-
     return -normalize(normal);
-}
-
-vec3 hsv2rgb(vec3 c) 
-{
-    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
 }
 
 void main()
@@ -227,8 +180,6 @@ void main()
     // trace ray
     int steps;
     VoxelCoord = VoxelTrace(eye, dir, steps);
-    float top = voxelTraceSteps;
-    float stepvisual = steps / top;
 
     // sample hue
     vec3 albedo = Sample(VoxelCoord);
@@ -252,8 +203,9 @@ void main()
     float shadow = 1;
     if (shadows)
     {
+        int sdwsteps;
         vec3 startPos = VoxelCoord + lightdir + (normal * shadowBias);
-        vec3 shadowVoxel = VoxelTrace(startPos, lightdir, steps);
+        vec3 shadowVoxel = VoxelTrace(startPos, lightdir, sdwsteps);
         if (shadowVoxel != vec3(0)) shadow = 0;
     }
     diffuse *= shadow;
@@ -277,7 +229,7 @@ void main()
     if (showDebugView)
     {
         if (debugView == 0) fragColor = vec4(normal * 0.5 + 0.5, 1.0);
-        if (debugView == 1) fragColor = vec4(bgc.x + stepvisual, bgc.y, bgc.z, 1);
+        if (debugView == 1) fragColor = vec4(bgc.x + steps / float(voxelTraceSteps), bgc.y, bgc.z, 1);
         if (debugView == 2) fragColor = vec4(1) * ao;
         return;
     }
